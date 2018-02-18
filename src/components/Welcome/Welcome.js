69e48c7e8465205e586042c9fd2350672c3f5e46
Esteban <garciawarneckee@gmail.com>
2018-02-18 15:23:57 -0300

    add constant colors and encapusalte creation of welcome messages in separated functions

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,31 +2,20 @@ import React, { Component } from 'react';
 import Typed from 'react-typed';
 
 import './Welcome.css';
+import Colors from '../../theme/Colors'
 
 export default class Welcome extends Component {
     render() {
-      /* VSCode colors. @TODO: add as Sass variables in a separate file  */
-      const keyWord = '#497EC7'
-      const variableName = '#9CD9EC'
-      const importName = '#4EC69D'
-      const functions = '#DCDCAA'
-
-      let _let = 'let '.fontcolor(keyWord)
-      let greetings = 'greetings '.fontcolor(variableName)
-      let Esteban = 'Esteban'.fontcolor(importName)
-      let welcomeMessage = 'welcomeMessage()'.fontcolor(functions)
-
-      let openBracket = '{ '.fontcolor(keyWord)
-      let _this = 'this'.fontcolor(keyWord)
-      let closeBracket = ' }'.fontcolor(keyWord)
+      const firstMessage = this.firstMessage()
+      const secondMessage = this.secondMessage()
 
         return (
           <div className="Welcome">
             <header className="Welcome-header">
               <Typed 
                 strings={[
-                  _let + greetings + '= ' + Esteban + '.' + welcomeMessage, 
-                  openBracket + _this + '.' + greetings + closeBracket, 
+                  firstMessage, 
+                  secondMessage,
                   'Hey there, glad you to be here!']} 
                    typeSpeed={100}
                    style= { { 'font-size': 36 } } 
@@ -40,5 +29,21 @@ export default class Welcome extends Component {
       }
     
       sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+      firstMessage() {
+        let _let = 'let '.fontcolor(Colors.vsCodeKeyword);
+        let greetings = 'greetings '.fontcolor(Colors.vsCodeVariableName);
+        let Esteban = 'Esteban'.fontcolor(Colors.vsCodeImportName);
+        let welcomeMessage = 'welcomeMessage()'.fontcolor(Colors.vsCodeFunctions);
+        return _let + greetings + '= ' + Esteban + '.' + welcomeMessage;
+      }
+
+      secondMessage() {
+        let openBracket = '{ '.fontcolor(Colors.vsCodeKeyword);
+        let _this = 'this'.fontcolor(Colors.vsCodeKeyword);
+        let greetings = 'greetings '.fontcolor(Colors.vsCodeVariableName);
+        let closeBracket = ' }'.fontcolor(Colors.vsCodeKeyword);
+        return openBracket + _this + '.' + greetings + closeBracket;
+      }
 
 }
---
+++ src/theme/Colors.js
@@ -0,0 +1,8 @@
+export const Colors = {
+    vsCodeKeyword: '#497EC7',
+    vsCodeVariableName: '#9CD9EC',
+    vsCodeImportName: '#4EC69D',
+    vsCodeFunctions: '#DCDCAA'
+} 
+
+export default Colors;
