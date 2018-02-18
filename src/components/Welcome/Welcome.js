598ad43ff93b65141bb66a981e14a60b6827e85a
Esteban <garciawarneckee@gmail.com>
2018-02-18 15:09:52 -0300

    add colors vscode colors to welcome message

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -5,14 +5,29 @@ import './Welcome.css';
 
 export default class Welcome extends Component {
     render() {
+      /* VSCode colors. @TODO: add as Sass variables in a separate file  */
+      const keyWord = '#497EC7'
+      const variableName = '#9CD9EC'
+      const importName = '#4EC69D'
+      const functions = '#DCDCAA'
+
+      let _let = 'let '.fontcolor(keyWord)
+      let greetings = 'greetings '.fontcolor(variableName)
+      let Esteban = 'Esteban'.fontcolor(importName)
+      let welcomeMessage = 'welcomeMessage()'.fontcolor(functions)
+
+      let openBracket = '{ '.fontcolor(keyWord)
+      let _this = 'this'.fontcolor(keyWord)
+      let closeBracket = ' }'.fontcolor(keyWord)
+
         return (
           <div className="Welcome">
             <header className="Welcome-header">
               <Typed 
                 strings={[
-                  'let greetings = Esteban.welcomeMessage()', 
-                  '{ this.greetings }', 
-                  'Hey there!']} 
+                  _let + greetings + '= ' + Esteban + '.' + welcomeMessage, 
+                  openBracket + _this + '.' + greetings + closeBracket, 
+                  'Hey there, glad you to be here!']} 
                    typeSpeed={100}
                    style= { { 'font-size': 36 } } 
                    className="Welcome-intro Welcome-title Centered-content"
@@ -24,7 +39,6 @@ export default class Welcome extends Component {
         );
       }
     
-      sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-      }
+      sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
 }
